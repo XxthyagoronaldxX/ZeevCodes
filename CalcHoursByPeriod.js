db017d5ccc91a9ad8821387a2247630f5fc964b7
Thyago Ronald <thyago.silva@netbiis.com>
2024-08-13 11:24:05 -0300

    Adicionando função que calcula horas por um período de data.

---
+++ CalcHoursByPeriod.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', async function(event) {
+  const startDateElement = document.getElementById("inpdataDeInicio");
+  const endDateElement = document.getElementById("inpdataFinal");
+  const totalHoursAccElement = document.getElementById("inptotalDeHorasUtilizadas");
+
+  function transformDDMMYYYInDate(dateStr) {
+    const [day, month, year] = dateStr.split("/");
+
+    return new Date(year, month - 1, day);
+  }
+
+  function transformDateInDDMMYYYY(date) {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+
+    return day + "/" + month + "/" + year;
+  }
+
+  function getDaysDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+
+    const dayDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    return dayDiff;
+  }
+
+  function calcTotalHoursByPeriod(startDateStr, endDateStr) {
+    if (endDateStr === "" || startDateStr === "") return;
+
+    let startDate = transformDDMMYYYInDate(startDateStr);
+    let endDate = transformDDMMYYYInDate(endDateStr);
+
+    if (startDate > endDate) {
+      endDate = startDate;
+      endDateElement.value = transformDateInDDMMYYYY(endDate);
+    }
+
+    const days = getDaysDifference(startDate, endDate);
+
+    totalHoursAccElement.value = (days + 1) * 8;
+  }
+
+  startDateElement.addEventListener("change", (event) => {
+    calcTotalHoursByPeriod(event.target.value, endDateElement.value);
+  });
+
+  endDateElement.addEventListener("change", (event) => {
+    calcTotalHoursByPeriod(startDateElement.value, event.target.value);
+  });
+});
